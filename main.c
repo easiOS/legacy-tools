25af479b534ae41d3f052e19289fdbf6244fc231
Easimer <easimer@gmail.com>
2016-01-06 20:16:05 +0100

    Rewrite program, change magic number

--- main.c
+++ main.c
@@ -6,10 +6,12 @@
 #include <string.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define FS_FILE        0x01
 #define FS_DIRECTORY   0x02
-//these are invalid
 #define FS_CHARDEVICE  0x03
 #define FS_BLOCKDEVICE 0x04
 #define FS_PIPE        0x05
@@ -23,66 +25,84 @@ struct initramfs_file {
 } __attribute__((packed));
 
 struct initramfs_header {
-  uint32_t magic; //0xC0C0A123
+  uint32_t magic; //0x45524653
   uint32_t count;
-  struct initramfs_file files[32];
+  struct initramfs_file files[0];
 } __attribute__((packed));
 
-int main(int argc, char** argv)
+int main(int argc, char** argv) // ./mkinitramfs indir outfile
 {
-  FILE* outfile = fopen("initramfs.bin", "wb");
-  if(!outfile) return 1;
-  DIR* dir;
-  struct dirent* entry;
-  uint32_t i = 0;
-  uint32_t filecount = 0;
-  if(!(dir = opendir("initramfs"))) return 1;
-  if(!(entry = readdir(dir))) return 1;
-  do
+  if(argc < 3)
   {
-    if(entry->d_type == DT_REG)
-      filecount++;
-  } while(entry = readdir(dir));
-  closedir(dir);
-  printf("Filecount: %d\n", filecount);
-  header.count = filecount;
-  header.magic = 0xC0C0A123;
-  fwrite(&header, sizeof(struct initramfs_header), 1, outfile);
-  //second round
-  dir = opendir("initramfs");
-  entry = readdir(dir);
-  uint32_t i = 0;
-  do
+    printf("Usage: %s indir outfile\n", argv[0]);
+    return 1;
+  }
+  char* dirname = argv[1];
+  char* outname = argv[2];
+  //initramfs fejléc
+  struct initramfs_header header;
+  header.magic = 0x45524653;
+  header.count = 0;
+
+  FILE* outf; //output file
+  DIR *dp; //directory
+  struct dirent *ep; //directory entry
+
+  dp = opendir(dirname); //opens the directory
+
+  if(!dp)
   {
-    if(entry->d_type == DT_REG)
-    {
-      struct initramfs_file* hf = &(header.files[i]);
-      char fn[256];
-      memset(fn, 0, 256);
-      strcat(fn, "initramfs/");
-      strcat(fn, file.name);
-      printf("Processing %s\n", fn);
-      FILE* entry_f = fopen(fn, "rb");
-      if(!entry_f) break;
-      printf("Reading size..");
-      fseek(entry_f, 0, SEEK_END);
-      file.size = ftell(entry_f);
-      fseek(entry_f, 0, SEEK_SET);
-      printf(".\n");
-      fwrite(&file, sizeof(struct initramfs_file), 1, outfile);
-      printf("Copying file...\n"); fflush(stdout);
-      int read = 0;
-      char buffer[128];
-      char ch;
-      while((ch = fgetc(entry_f)) != EOF)
-        fputc(ch, outfile);
-      printf("\nDone!\n");
-      fflush(entry_f);
-      fclose(entry_f);
-      fflush(outfile);
-    }
-  } while(entry = readdir(dir));
-  fclose(outfile);
-  printf("Exit!\n");
+    perror("Can't open directory");
+    return 1;
+  }
+
+  //count files
+
+  while(ep = readdir(dp))
+  {
+    if(ep->d_name[0] == '.') //skip ., .. and dotfiles
+      continue;
+    header.count++;
+  }
+
+  printf("Count: %d\n", header.count);
+
+  outf = fopen(outname, "wb");
+  fwrite(&header, sizeof(struct initramfs_header), 1, outf);
+
+  rewinddir(dp);
+
+  while(ep = readdir(dp))
+  {
+    if(ep->d_name[0] == '.') //skip ., .. and dotfiles
+      continue;
+    struct initramfs_file file;
+    char fullpath[256];
+    memset(&fullpath, 0, 256);
+    memset(&file, 0, sizeof(struct initramfs_file));
+    //set path
+    strcat(fullpath, dirname);
+    strcat(fullpath, "/");
+    strcat(fullpath, ep->d_name);
+    //get size
+    struct stat finfo;
+    stat(fullpath, &finfo);
+    file.size = finfo.st_size;
+    //copy filename
+    strncpy((char*)&file.name, ep->d_name, 128);
+    //write file header
+    fwrite(&file, sizeof(struct initramfs_file), 1, outf);
+    //write the file itself
+    FILE* inf = fopen(fullpath, "rb");
+    char buffer[file.size];
+    fread(&buffer, file.size, 1, inf);
+    fclose(inf);
+    fwrite(&buffer, file.size, 1, outf);
+    printf("%s\n", fullpath);
+    printf("\tSize: %d\n", file.size);
+  }
+  fclose(outf);
+  closedir(dp);
+
   return 0;
 }
